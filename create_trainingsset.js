5f89e16a109f1f72f16e266420eec8d52d6ab053
Lars Gregori <lars.gregori@sap.com>
2018-10-28 22:01:25 +0100

    add validation; clear path

--- create_trainingsset.js
+++ create_trainingsset.js
@@ -8,28 +8,34 @@ let labelsJson = fs.readFileSync("./labels.json", "utf8").split("\n");
 labelsJson.forEach((line) => {
   let kvs = line.split(":");
   if (kvs.length > 1) {
-    labels[kvs[0]] = kvs[1].split("'")[1];
+    let val = kvs[1].split("'")[1].toLowerCase().replace(/,/gi, "_").replace(/ /gi, "_");
+    labels[kvs[0]] = val;
   }
 });
 
 const PATH = "fashion_data";
 const TRAIN_PATH = "training";
 const TEST_PATH = "test";
+const VALID_PATH = "validation";
 
 _mkdir(PATH);
 _mkdir(PATH + "/" + TRAIN_PATH);
 _mkdir(PATH + "/" + TEST_PATH);
+_mkdir(PATH + "/" + VALID_PATH);
+
 
 Object.values(labels).forEach((label) => {
   _mkdir(PATH + "/" + TRAIN_PATH + "/" + label);
   _mkdir(PATH + "/" + TEST_PATH + "/" + label);
+  _mkdir(PATH + "/" + VALID_PATH + "/" + label);
 });
+return 
 
 let train = fs.readFileSync("./train.txt", "utf8").split("\n");
 _copyFiles(train, TRAIN_PATH);
 
 let test = fs.readFileSync("./test.txt", "utf8").split("\n");
-_copyFiles(test, TEST_PATH);
+_copyFiles(test, TEST_PATH, VALID_PATH);
 
 function _mkdir(path) {
   try {
@@ -41,7 +47,8 @@ function _mkdir(path) {
   }
 }
 
-function _copyFiles(filelist, target) {
+function _copyFiles(filelist, target, target2) {
+  var useTarget2 = false;
   filelist.forEach((file) => {
 
     if (file && file.length > 0) {
@@ -50,7 +57,14 @@ function _copyFiles(filelist, target) {
       let label = labels[ss[0]];
 
       if (label && label.length > 0) {
-        let dest = PATH + "/" + target + "/" + label + "/" + ss[ss.length - 1];
+        var t = target;
+        if (useTarget2) {
+          t = target2;
+        }
+        if (target2) {
+          useTarget2 = !useTarget2;
+        }
+        let dest = PATH + "/" + t + "/" + label + "/" + ss[ss.length - 1];
         fs.copyFile("images/" + source, dest, (err) => {
           if (err) {
             console.log(err);
